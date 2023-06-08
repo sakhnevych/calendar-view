e1a8c6e81cd2e7ed3d151cf1ac1526c37a2d0063
Oleksandr Sakhnevych <olsakhnevych@gmail.com>
2023-06-08 23:12:25 +0300

    - Fix a bug where notes were not displayed when split by day (#17); - Rename the git repository.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.rst", "r") as f:
 
 setuptools.setup(
     name="calendar-view",
-    version="2.2.0",
+    version="2.3.0",
     author="Oleksandr Sakhnevych, Daniil Limariev",
     author_email="o.sakhnevych@gmail.com, danillim02102003@gmail.com",
     description="Library provides a graphical view of the calendar.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/sakhnevych/CalendarView",
+    url="https://github.com/sakhnevych/calendar-view",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
