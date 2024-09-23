70faa100a64200e41eda02228fac21ea444e48d7
Oleksandr Sakhnevych <olsakhnevych@gmail.com>
2024-09-23 22:43:45 +0300

    #32: Update changelog and the version for the 2.4.5 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="calendar-view",
-    version="2.4.4",
+    version="2.4.5",
     author="Oleksandr Sakhnevych, Daniil Limariev",
     author_email="o.sakhnevych@gmail.com, danillim02102003@gmail.com",
     description="Library provides a graphical view of the calendar.",
